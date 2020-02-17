bed63aaa5b4c024ca38f2e9c5d3d3ff972358e2a
Bowie <bowie@thinkful.com>
2020-02-16 16:03:11 -0800

    change endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,3 @@
 export default {
-    // API_ENDPOINT: 'https://aqueous-wave-46723.herokuapp.com'
-    API_ENDPOINT: 'http://localhost:8000'
-
+    API_ENDPOINT: 'https://aqueous-wave-46723.herokuapp.com'
 }
